339e05fac8003f5f583b2a3e4f8fae45eafdd66c
son1999 <nguyenthaison12c11021999@gmail.com>
2022-04-12 11:43:51 +0700

    update authentication, database, posts, users init pr

---
+++ src/authentication/dto/register.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsEmail,
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  Matches,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  fullName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(7)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^\+[1-9]\d{1,14}$/)
+  phoneNumber: string;
+}
+
+export default RegisterDto;
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,65 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/createPost.dto';
+import { Post } from './types/post.interface';
+import { UpdatePostDto } from './dto/updatePost.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import PostEntity from './entity/post.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export default class PostsService {
+  private lastPostId = 0;
+  private posts: Post[] = [];
+
+  constructor(
+    @InjectRepository(PostEntity)
+    private postsRepository: Repository<PostEntity>,
+  ) {}
+
+  getAllPosts() {
+    return this.postsRepository.find();
+  }
+
+  async getPostById(id: number) {
+    const post = await this.postsRepository.findOne({
+      where: { id: id },
+    });
+    if (post) {
+      return post;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  replacePost(id: number, post: UpdatePostDto) {
+    const postIndex = this.posts.findIndex((post) => post.id === id);
+    if (postIndex > -1) {
+      this.posts[postIndex] = post;
+      return post;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  async updatePost(id: number, post: UpdatePostDto) {
+    await this.postsRepository.update(id, post);
+    const updatedPost = await this.postsRepository.findOne({
+      where: { id: id },
+    });
+    if (updatedPost) {
+      return updatedPost;
+    }
+    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+  }
+
+  async createPost(post: CreatePostDto) {
+    const newPost = await this.postsRepository.create(post);
+    await this.postsRepository.save(newPost);
+    return newPost;
+  }
+
+  async deletePost(id: number) {
+    const deleteResponse = await this.postsRepository.delete(id);
+    if (!deleteResponse.affected) {
+      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    }
+  }
+}
